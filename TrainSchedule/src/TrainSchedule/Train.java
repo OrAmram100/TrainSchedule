c99a0301a54952701070a75a71f90d0cfc91a353
khizv <khizv@RoniKhizverg-PC>
2020-07-31 15:38:12 +0300

    i did a menu

--- TrainSchedule/src/TrainSchedule/Train.java
+++ TrainSchedule/src/TrainSchedule/Train.java
@@ -1,11 +1,45 @@
 package TrainSchedule;
 
+import java.util.Scanner;
+
 public class Train {
 
 	public static void main(String[] args) {
-		int or=1234;
-		int roni=1111111;
+		Scanner scan = new Scanner(System.in);
+
+		int choice = 0;
+
+		do {
+
+			choice= scan.nextInt();
+
+			switch(choice) {
+
+			case 1: 
+
+				break;
+
+
+			case 2: 
+
+				
+				break;
+
+			
+			case 9: // Exit:
+				System.out.println("Exit.\nThank you for using.\nhave a great day !");
+				break;
+
+
+			default:
+				System.out.println("This choise does not exist in the menu"); // case choice is not valid
+				break;
+			}
+		} while (choice != 9);
 
 	}
+
+}
+
 
 }
