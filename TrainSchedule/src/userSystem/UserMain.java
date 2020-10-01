a9b0d602a7eb798ad3062347363cd9eb47a63d34
afeka <afeka@Ubuntu19V4-14.academic.management.afeka.local>
2020-10-01 21:12:59 +0300

    //added outformat

--- TrainSchedule/src/userSystem/UserMain.java
+++ TrainSchedule/src/userSystem/UserMain.java
@@ -11,15 +11,20 @@ import TrainSchedule.RidesManagement;
 public class UserMain implements IsearchRides {
 
 	public static void main(String[] args) throws Exception {
-		String out = new SimpleDateFormat("yyyy-MM-dd '.txt'").format(new Date());
-		File f = new File("/home/afeka/git/TrainSchedule/TrainSchedule/railWay " + out);
-		Scanner s = new Scanner(f);
-		RidesManagement.FiletoUserInsertion(s);
-		int hour = Integer.parseInt(args[2]);
-		int minute = Integer.parseInt(args[3]);
-		RidesManagement.sortAll();
-		RidesManagement.stringRides();
-		IsearchRides.search(args[0], args[1], hour, minute);
-	}
+		boolean isHtml = args[0].equalsIgnoreCase("html") & args.length > 0;
+		if (!isHtml) {
+			String out = new SimpleDateFormat("yyyy-MM-dd '.txt'").format(new Date());
+			File f = new File("/home/afeka/git/TrainSchedule/TrainSchedule/railWay " + out);
+			Scanner s = new Scanner(f);
+			RidesManagement.FiletoUserInsertion(s);
+			int hour = Integer.parseInt(args[3]);
+			int minute = Integer.parseInt(args[4]);
+			RidesManagement.sortAll();
+			RidesManagement.stringRides();
+			IsearchRides.search(args[1], args[2], hour, minute);
+		} else {
+			
+		}
 
+	}
 }
