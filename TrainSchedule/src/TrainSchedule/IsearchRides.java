d9950287df8e8ea8da846bd586eff30914a98544
oramr <oramr@DESKTOP-E5I505B>
2020-10-02 15:15:39 +0300

    //added rides in the file trainschedule and improved the output

--- TrainSchedule/src/TrainSchedule/IsearchRides.java
+++ TrainSchedule/src/TrainSchedule/IsearchRides.java
@@ -6,7 +6,7 @@ public interface IsearchRides {
 		
 		String route = RidesManagement.LocateRide(destination, station, hour, minutes);
 		if (!route.equals(""))
-			System.out.println("Station " + station + " --> Station " + destination + "\n" + route);
+			System.out.println(route);
 		else
 			System.out.println("Sorry there is no rides for your request today!!");
 		
